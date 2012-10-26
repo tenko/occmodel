fcfc49ed6e21d06b21c2a5aa346cda1c31e1707d
Runar Tenfjord <runar.tenfjord@tenko.no>
2012-10-26 21:29:44 +0200

    Simplified GLSL material

--- occmodel/occmodelviewer.py
+++ occmodel/occmodelviewer.py
@@ -62,26 +62,6 @@ GLSL_FRAG_PONG = \
 varying vec3 normal;
 varying vec3 vertex;
 
-const vec4 AMBIENT_BLACK = vec4(0.0, 0.0, 0.0, 1.0);
-const vec4 DEFAULT_BLACK = vec4(0.0, 0.0, 0.0, 0.0);
-
-bool isLightEnabled(in int i)
-{
-    // A separate variable is used to get
-    // rid of a linker error.
-    bool enabled = true;
-   
-    // If all the colors of the Light are set
-    // to BLACK then we know we don't need to bother
-    // doing a lighting calculation on it.
-    if ((gl_LightSource[i].ambient  == AMBIENT_BLACK) &&
-        (gl_LightSource[i].diffuse  == DEFAULT_BLACK) &&
-        (gl_LightSource[i].specular == DEFAULT_BLACK))
-        enabled = false;
-       
-    return(enabled);
-}
-
 float calculateAttenuation(in int i, in float dist)
 {
     return(1.0 / (gl_LightSource[i].constantAttenuation +
@@ -169,19 +149,16 @@ void spotLight(in int i, in vec3 N, in vec3 V, in float shininess,
 void calculateLighting(in vec3 N, in vec3 V, in float shininess,
                        inout vec4 ambient, inout vec4 diffuse, inout vec4 specular)
 {
-    // Just loop through each light, and if its enabled add
+    // Just loop through each light, and add
     // its contributions to the color of the pixel.
     for (int i = 0; i < MAX_LIGHTS - 1; i++)
     {
-        if (isLightEnabled(i))
-        {
-            if (gl_LightSource[i].position.w == 0.0)
-                directionalLight(i, N, shininess, ambient, diffuse, specular);
-            else if (gl_LightSource[i].spotCutoff == 180.0)
-                pointLight(i, N, V, shininess, ambient, diffuse, specular);
-            else
-                 spotLight(i, N, V, shininess, ambient, diffuse, specular);
-        }
+        if (gl_LightSource[i].position.w == 0.0)
+            directionalLight(i, N, shininess, ambient, diffuse, specular);
+        else if (gl_LightSource[i].spotCutoff == 180.0)
+            pointLight(i, N, V, shininess, ambient, diffuse, specular);
+        else
+             spotLight(i, N, V, shininess, ambient, diffuse, specular);
     }
 }
 
